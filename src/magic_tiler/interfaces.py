b055ef58b293155032731065dc6ed91977d4d67b
abstractlyZach <zach3lee@gmail.com>
2021-05-16 13:11:13 -0700

    Add pre-commit

--- src/magic_tiler/interfaces.py
+++ src/magic_tiler/interfaces.py
@@ -3,17 +3,17 @@ import abc
 
 class TilingWindowManager(object):
     @abc.abstractmethod
-    def make_horizontal_sibling(window_title_regex: str, command) -> None:
+    def make_horizontal_sibling(self, window_title_regex: str, command) -> None:
         pass
 
     @abc.abstractmethod
-    def make_vertical_sibling(window_title_regex: str, command) -> None:
+    def make_vertical_sibling(self, window_title_regex: str, command) -> None:
         pass
 
     @abc.abstractmethod
-    def resize_width(window_title_regex: str, container_percentage: int) -> None:
+    def resize_width(self, window_title_regex: str, container_percentage: int) -> None:
         pass
 
     @abc.abstractmethod
-    def resize_height(window_title_regex: str, container_percentage: int) -> None:
+    def resize_height(self, window_title_regex: str, container_percentage: int) -> None:
         pass
