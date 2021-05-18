4bf516bb619076f046ed690e1a30991b544a9038
abstractlyZach <zach3lee@gmail.com>
2021-05-18 13:08:00 -0700

    Add main script as a click/poetry script

--- src/magic_tiler/main.py
+++ src/magic_tiler/main.py
@@ -1,10 +1,17 @@
 import logging
 
+import click
+
+import magic_tiler
 from magic_tiler import subprocess_runner
 from magic_tiler import sway
 
-logging.basicConfig(level=logging.INFO)
-swaywm = sway.Sway(subprocess_runner.SubprocessRunner())
-swaywm.make_horizontal_sibling("Alacritty:v", 'alacritty -e sh -c "ls | fzf"')
-# how do we make alacritty hang around after running the initial command?
-swaywm.make_horizontal_sibling("Alacritty:poetry", 'alacritty -e zsh -c "ls"')
+
+@click.command()
+@click.version_option(version=magic_tiler.__version__)
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    swaywm = sway.Sway(subprocess_runner.SubprocessRunner())
+    swaywm.make_horizontal_sibling("Alacritty:v", 'alacritty -e sh -c "ls | fzf"')
+    # how do we make alacritty hang around after running the initial command?
+    swaywm.make_horizontal_sibling("Alacritty:poetry", 'alacritty -e zsh -c "ls"')
