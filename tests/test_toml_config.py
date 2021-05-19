e264f65e0309e437e2ecd88599bfca7f919e883d
abstractlyZach <zach3lee@gmail.com>
2021-05-18 17:51:54 -0700

    Rewrite example toml to use arrays of tables

--- tests/test_toml_config.py
+++ tests/test_toml_config.py
@@ -4,12 +4,10 @@ from magic_tiler import toml_config
 def test_toml():
     config_reader = toml_config.TomlConfig()
     config = config_reader.read("examples/centered_big.toml")
-    assert config["left-panel"] == {
-        "size": 25,
-        "command": "alacritty --title left-panel",
-    }
     assert config == {
-        "left-panel": {"size": 25, "command": "alacritty --title left-panel"},
-        "middle-panel": {"size": 50, "command": "alacritty --title middle-panel"},
-        "right-panel": {"size": 25, "command": "alacritty --title right-panel"},
+        "children": [
+            {"size": 25, "command": "alacritty --title left-panel"},
+            {"size": 50, "command": "alacritty --title middle-panel"},
+            {"size": 25, "command": "alacritty --title right-panel"},
+        ]
     }
