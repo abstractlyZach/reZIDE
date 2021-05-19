dfd37c3f746674f19e049e93cc7d8e3d07ff4898
abstractlyZach <zach3lee@gmail.com>
2021-05-18 18:38:31 -0700

    Redesign the structure of the tree representation

--- tests/test_toml_config.py
+++ tests/test_toml_config.py
@@ -5,9 +5,17 @@ def test_toml():
     config_reader = toml_config.TomlConfig()
     config = config_reader.read("examples/centered_big.toml")
     assert config == {
-        "children": [
-            {"size": 25, "command": "alacritty --title left-panel"},
+        "screen": [
+            {
+                "children": [
+                    {"size": 60, "command": "alacritty --title medium-window"},
+                    {"size": 40, "command": "alacritty --title tiny-window"},
+                ],
+                "split": "horizontal",
+                "size": 25,
+            },
             {"size": 50, "command": "alacritty --title middle-panel"},
             {"size": 25, "command": "alacritty --title right-panel"},
-        ]
+        ],
+        "split": "vertical",
     }
