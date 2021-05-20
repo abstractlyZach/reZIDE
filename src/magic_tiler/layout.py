9dcdfcb7b8576879a3bce7e40e31a983a3905135
abstractlyZach <zach3lee@gmail.com>
2021-05-20 09:18:48 -0700

    Create custom-defined user IDs for windows

--- src/magic_tiler/layout.py
+++ src/magic_tiler/layout.py
@@ -1,16 +1,37 @@
-from typing import Dict
+from typing import Dict, Set
 
 from magic_tiler import interfaces
 
 
 class Layout(object):
+    """Convert a configuration into a collection of Tiles"""
+
     def __init__(
         self, config_reader: interfaces.ConfigReader, layout_name: str
     ) -> None:
-        self._config = config_reader.to_dict()
         self._windows: Dict[int, Dict] = dict()
+        self._parse_config(config_reader.to_dict()[layout_name])
+
+    def _parse_config(self, layout_dict: Dict) -> None:
+        self._reserved_ids: Set = set()
         self._next_window_id: int = 0
-        self._parse_node(self._config[layout_name])
+        self._get_reserved_ids(layout_dict)
+        self._parse_node(layout_dict)
+
+    def _get_reserved_ids(self, node: Dict) -> None:
+        """Recursively parse a node and its children using depth-first traversal
+        to get the user-defined IDs and reserve them.
+        """
+        if "children" in node:
+            for child_node in node["children"]:
+                self._get_reserved_ids(child_node)
+        else:  # process the node if it's a leaf
+            if "id" in node:  # custom user-defined id
+                if node["id"] in self._reserved_ids:
+                    raise KeyError(
+                        "There are multiple windows with the same ID in your config"
+                    )
+                self._reserved_ids.add(node["id"])
 
     def _parse_node(self, node: Dict) -> None:
         """Recursively process a node and its children using depth-first traversal"""
@@ -18,8 +39,13 @@ class Layout(object):
             for child_node in node["children"]:
                 self._parse_node(child_node)
         else:  # process the node if it's a leaf
-            self._windows[self._next_window_id] = {"command": node["command"]}
-            self._next_window_id += 1
+            if "id" in node:  # custom user-defined id
+                self._windows[node["id"]] = {"command": node["command"]}
+            else:
+                while self._next_window_id in self._reserved_ids:
+                    self._next_window_id += 1
+                self._windows[self._next_window_id] = {"command": node["command"]}
+                self._next_window_id += 1
 
     @property
     def windows(self) -> Dict:
--- tests/test_layout.py
+++ tests/test_layout.py
@@ -63,6 +63,49 @@ layout_test_cases = [
             2: {"command": "alacritty --title right-panel"},
         },
     ),
+    # Automatically assign IDs only to windows that don't have their own IDs
+    LayoutTestCase(
+        config={
+            "screen": {
+                "children": [
+                    {"size": 25, "command": "alacritty --title left-panel", "id": 50},
+                    {"size": 50, "command": "alacritty --title middle-panel"},
+                    {"size": 25, "command": "alacritty --title right-panel"},
+                ],
+                "split": "horizontal",
+            }
+        },
+        expected_windows={
+            50: {"command": "alacritty --title left-panel"},
+            0: {"command": "alacritty --title middle-panel"},
+            1: {"command": "alacritty --title right-panel"},
+        },
+    ),
+    # automatically assigned IDs shouldn't collide with user-defined IDs
+    # so we shouldn't tag the first window with ID 0
+    LayoutTestCase(
+        config={
+            "screen": {
+                "children": [
+                    {"size": 25, "command": "alacritty --title left-panel"},
+                    {
+                        "size": 50,
+                        "command": "alacritty --title middle-left-panel",
+                        "id": 0,
+                    },
+                    {"size": 50, "command": "alacritty --title middle-right-panel"},
+                    {"size": 25, "command": "alacritty --title right-panel"},
+                ],
+                "split": "horizontal",
+            }
+        },
+        expected_windows={
+            1: {"command": "alacritty --title left-panel"},
+            0: {"command": "alacritty --title middle-left-panel"},
+            2: {"command": "alacritty --title middle-right-panel"},
+            3: {"command": "alacritty --title right-panel"},
+        },
+    ),
 ]
 
 
@@ -70,3 +113,23 @@ layout_test_cases = [
 def test_layout(test_case):
     mylayout = layout.Layout(FakeConfig(test_case.config), "screen")
     assert mylayout.windows == test_case.expected_windows
+
+
+def test_duplicate_ids_raise_exception():
+    config = {
+        "screen": {
+            "children": [
+                {"size": 25, "command": "alacritty --title left-panel", "id": 17},
+                {
+                    "size": 50,
+                    "command": "alacritty --title middle-left-panel",
+                    "id": 17,
+                },
+                {"size": 50, "command": "alacritty --title middle-right-panel"},
+                {"size": 25, "command": "alacritty --title right-panel"},
+            ],
+            "split": "horizontal",
+        }
+    }
+    with pytest.raises(KeyError):
+        layout.Layout(FakeConfig(config), "screen")
