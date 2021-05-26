79b40a942d4f06fa3aa4bdf483dbe414ab6e6a13
abstractlyZach <zach3lee@gmail.com>
2021-05-26 08:02:05 -0700

    Add verbosity option to main script

--- src/magic_tiler/main.py
+++ src/magic_tiler/main.py
@@ -8,11 +8,27 @@ from magic_tiler import dtos
 from magic_tiler import subprocess_runner
 from magic_tiler import sway
 
+# maps from verbosity level to log levels
+VERBOSITY_LOG_LEVELS = {
+    0: logging.WARNING,
+    1: logging.INFO,
+    2: logging.DEBUG,
+}
+
 
 @click.command()
+@click.option(
+    "-v",
+    "--verbose",
+    "verbosity_level",
+    default=0,
+    count=True,
+    help="Set verbosity. Add more v's to increase verbosity. For example, -v is "
+    + "verbosity level 1 and -vv is verbosity level 2",
+)
 @click.version_option(version=magic_tiler.__version__)
-def main() -> None:
-    logging.basicConfig(level=logging.DEBUG)
+def main(verbosity_level: int) -> None:
+    logging.basicConfig(level=VERBOSITY_LOG_LEVELS[verbosity_level])
     swaywm = sway.Sway(subprocess_runner.SubprocessRunner())
     logging.debug(
         f"{swaywm.num_workspace_windows} windows are open in the current workspace"
