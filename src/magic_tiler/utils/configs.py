5f7e3573743a148ebc8acb9e30fcd3c5b2854fdd
abstractlyZach <zach3lee@gmail.com>
2021-05-31 14:17:40 -0700

    refactor config finding logic

--- src/magic_tiler/utils/configs.py
+++ src/magic_tiler/utils/configs.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import Dict
 
@@ -9,13 +10,20 @@ from magic_tiler.utils import interfaces
 
 class TomlConfig(interfaces.ConfigReader):
     def __init__(self, filestore: interfaces.FileStore, env: dtos.Env) -> None:
+        paths_to_check = []
         if env.xdg_config_home:
-            target_path = os.path.join(
-                env.xdg_config_home, "magic_tiler", "config.toml"
-            )
+            path = os.path.join(env.xdg_config_home, "magic_tiler", "config.toml")
+            paths_to_check.append(path)
         else:
-            target_path = os.path.join(env.home, ".magic_tiler.toml")
-        if not filestore.path_exists(target_path):
+            path = os.path.join(env.home, ".magic_tiler.toml")
+            paths_to_check.append(path)
+        for path in paths_to_check:
+            logging.debug(f"checking {path}")
+            if filestore.path_exists(path):
+                target_path = path
+                logging.debug(f"found config at {path}")
+                break
+        else:
             raise RuntimeError(
                 'Could not find config file at "$XDG_CONFIG_HOME/magic_tiler/config.toml"'
                 + 'or "$HOME/.magic_tiler.toml"'
