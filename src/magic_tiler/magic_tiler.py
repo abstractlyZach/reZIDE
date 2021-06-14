81e8ee74d1fe78d609a995848df43e52d56b4160
abstractlyZach <zach3lee@gmail.com>
2021-06-14 09:03:40 -0700

    Add help text

--- src/magic_tiler/magic_tiler.py
+++ src/magic_tiler/magic_tiler.py
@@ -31,13 +31,25 @@ VERBOSITY_LOG_LEVELS = {
     help="Set verbosity. Add more v's to increase verbosity. For example, -v is "
     + "verbosity level 1 and -vv is verbosity level 2",
 )
-@click.option("-c", "--xdg-config-home-dir", envvar="XDG_CONFIG_HOME")
-@click.option("--user-home-dir", envvar="HOME")
+@click.option(
+    "-c",
+    "--xdg-config-home-dir",
+    envvar="XDG_CONFIG_HOME",
+    help="The directory for your XDG config files. Reads from the XDG_CONFIG_HOME"
+    + " environment variable by default.",
+)
+@click.option(
+    "--user-home-dir",
+    envvar="HOME",
+    help="The current user's home directory. Reads from the HOME environment variable"
+    + " by default.",
+)
 @click.argument("layout_name")
 @click.version_option(version=magic_tiler.__version__)
 def main(
     verbosity_level: int, xdg_config_home_dir: str, user_home_dir: str, layout_name: str
 ) -> None:
+    """Create the IDE registered at LAYOUT_NAME in the configuration file."""
     env = dtos.Env(home=user_home_dir, xdg_config_home=xdg_config_home_dir)
     config = configs.TomlConfig(filestore.LocalFilestore(), env=env)
     window_manager = sway.Sway()
