3dafecfe3109d3ac32119070517a33b62cdbe791
abstractlyZach <zach3lee@gmail.com>
2021-06-17 05:26:48 -0700

    extract window management from Layout

--- src/magic_tiler/utils/layouts.py
+++ src/magic_tiler/utils/layouts.py
@@ -1,6 +1,6 @@
 import collections
 import logging
-from typing import Dict, Set
+from typing import Dict, Iterable, Set
 
 from magic_tiler.utils import interfaces
 from magic_tiler.utils import tree
@@ -28,7 +28,7 @@ class LayoutManager(object):
         if "size" in self._root_node:
             raise RuntimeError("root node shouldn't have a size. size is implied 100")
         self._layout_has_been_selected = True
-        self._selected_layout = Layout(self._window_manager, self._root_node)
+        self._selected_layout = Layout(self._root_node)
         self._root_node["size"] = 100
         # TODO: should parse and validate tree here in the future
 
@@ -43,42 +43,30 @@ class LayoutManager(object):
             raise RuntimeError(
                 "There are multiple windows open in the current workspace."
             )
-        self._selected_layout.spawn_windows()
+        self._created_windows: Set[str] = set()
+        for window in self._selected_layout.zachstras_traversal():
+            if window.is_parent:
+                self._window_manager.split(window.data)
+            elif window.data.mark in self._created_windows:
+                self._window_manager.focus(window.data)
+            else:
+                self._window_manager.make_window(window.data)
+                self._created_windows.add(window.data.mark)
 
 
 class Layout(object):
-    # todo: extract window manager from layout's responsibilities
-    def __init__(
-        self, window_manager: interfaces.TilingWindowManager, root_node: Dict
-    ) -> None:
+    def __init__(self, root_node: Dict) -> None:
         self._tree = tree.create_tree(root_node)
-        self._window_manager = window_manager
 
-    def spawn_windows(self) -> None:
-        self._parse_tree(self._tree)
-
-    def _parse_tree(self, root_node: tree.TreeNode) -> None:
-        """Recursively parse the tree, creating, splitting, and focusing windows as
-        appropriate
-        """
-        node_queue = collections.deque([root_node])
-        self._created_windows: Set[str] = set()
+    def zachstras_traversal(self) -> Iterable[tree.TreeNode]:
+        node_queue = collections.deque([self._tree])
         while len(node_queue) >= 1:
             current_node = node_queue.popleft()
             logging.debug(f"dequeuing {current_node}")
             if current_node.is_parent:
-                self._attempt_to_create_leftmost_descendant(current_node.children[0])
-                self._window_manager.split(current_node.data)
+                yield current_node.children[0].get_leftmost_descendant()
+                yield current_node
                 for child in current_node.children[1:]:
-                    self._attempt_to_create_leftmost_descendant(child)
+                    yield child.get_leftmost_descendant()
                 for child in current_node.children:
                     node_queue.append(child)
-
-    def _attempt_to_create_leftmost_descendant(self, node: tree.TreeNode) -> None:
-        logging.debug(f"getting leftmost descendant of {node}")
-        leftmost_descendant = node.get_leftmost_descendant()
-        if leftmost_descendant.data.mark in self._created_windows:
-            self._window_manager.focus(leftmost_descendant.data)
-        else:
-            self._window_manager.make_window(leftmost_descendant.data)
-            self._created_windows.add(leftmost_descendant.data.mark)
