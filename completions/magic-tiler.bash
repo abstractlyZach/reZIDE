4bd0d8ae70ff266e0d9e3f3ca207a214e1935127
abstractlyZach <zach3lee@gmail.com>
2021-07-13 13:08:14 -0700

    shell completion (#52)
    * Add shell completion script
    
    * Add completion files
    
    * Add documentation for shell completion

---
+++ completions/magic-tiler.bash
@@ -0,0 +1,28 @@
+_magic_tiler_completion() {
+    local IFS=$'\n'
+    local response
+
+    response=$(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _MAGIC_TILER_COMPLETE=bash_complete $1)
+
+    for completion in $response; do
+        IFS=',' read type value <<< "$completion"
+
+        if [[ $type == 'dir' ]]; then
+            COMREPLY=()
+            compopt -o dirnames
+        elif [[ $type == 'file' ]]; then
+            COMREPLY=()
+            compopt -o default
+        elif [[ $type == 'plain' ]]; then
+            COMPREPLY+=($value)
+        fi
+    done
+
+    return 0
+}
+
+_magic_tiler_completion_setup() {
+    complete -o nosort -F _magic_tiler_completion magic-tiler
+}
+
+_magic_tiler_completion_setup;
