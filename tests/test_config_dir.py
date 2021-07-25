e7917e4ad42e65c510f54a75bd309ba3bc3d98cd
abstractlyZach <zach3lee@gmail.com>
2021-07-25 07:51:09 -0700

    Rewrite test cases to use namedtuples

--- tests/test_config_dir.py
+++ tests/test_config_dir.py
@@ -1,3 +1,5 @@
+from typing import Dict, NamedTuple, Set
+
 import pytest
 
 from rezide.utils import config_dir
@@ -10,24 +12,35 @@ def test_env():
     return dtos.Env(home="/home/test/", xdg_config_home="/home/test/.config/")
 
 
+class DirTestCase(NamedTuple):
+    files: Dict[str, str]
+    expected_layout_names: Set[str]
+
+
 specified_dir_tests = [
-    ({"/abc/alpha.toml": "", "/abc/beta.toml": ""}, {"alpha", "beta"}),
-    ({"/abc/hello.toml": "", "/abc/world.toml": ""}, {"hello", "world"}),
-    (
-        {
+    DirTestCase(
+        files={"/abc/alpha.toml": "", "/abc/beta.toml": ""},
+        expected_layout_names={"alpha", "beta"},
+    ),
+    DirTestCase(
+        files={"/abc/hello.toml": "", "/abc/world.toml": ""},
+        expected_layout_names={"hello", "world"},
+    ),
+    DirTestCase(
+        files={
             "/abc/hello.toml": "",
             "/abc/world.toml": "",
             "/home/test/.config/rezide/omg.toml": "",
         },
-        {"hello", "world"},
+        expected_layout_names={"hello", "world"},
     ),
-    (
-        {
+    DirTestCase(
+        files={
             "/abc/hello.toml": "",
             "/abc/world.toml": "",
             "/home/test/.rezide/omg.toml": "",
         },
-        {"hello", "world"},
+        expected_layout_names={"hello", "world"},
     ),
 ]
 
@@ -35,41 +48,41 @@ specified_dir_tests = [
 @pytest.mark.parametrize("test_case", specified_dir_tests)
 def test_lists_layouts_from_specified_dir(test_case, test_env):
     """If a dir is specified, give that highest priority"""
-    filestore = fakes.FakeFilestore(test_case[0])
+    filestore = fakes.FakeFilestore(test_case.files)
     dir = config_dir.ConfigDir(filestore, test_env, "/abc/")
-    assert dir.list_layouts() == test_case[1]
+    assert dir.list_layouts() == test_case.expected_layout_names
 
 
 @pytest.mark.parametrize("test_case", specified_dir_tests)
 def test_throws_error_if_specified_dir_doesnt_exist(test_case, test_env):
     """If the specified dir doesn't exist, throw an error"""
-    filestore = fakes.FakeFilestore(test_case[0])
+    filestore = fakes.FakeFilestore(test_case.files)
     with pytest.raises(RuntimeError):
         config_dir.ConfigDir(filestore, test_env, "/woopsies/")
 
 
 xdg_dir_tests = [
-    (
-        {
+    DirTestCase(
+        files={
             "/home/test/.config/rezide/alpha.toml": "",
             "/home/test/.config/rezide/beta.toml": "",
         },
-        {"alpha", "beta"},
+        expected_layout_names={"alpha", "beta"},
     ),
-    (
-        {
+    DirTestCase(
+        files={
             "/home/test/.config/rezide/hello.toml": "",
             "/home/test/.config/rezide/world.toml": "",
         },
-        {"hello", "world"},
+        expected_layout_names={"hello", "world"},
     ),
-    (
-        {
+    DirTestCase(
+        files={
             "/home/test/.config/rezide/hello.toml": "",
             "/home/test/.config/rezide/world.toml": "",
             "/home/test/.rezide/omg.toml": "",
         },
-        {"hello", "world"},
+        expected_layout_names={"hello", "world"},
     ),
 ]
 
@@ -77,19 +90,22 @@ xdg_dir_tests = [
 @pytest.mark.parametrize("test_case", xdg_dir_tests)
 def test_lists_layouts_from_xdg_config_home(test_case, test_env):
     """Look in $XDG_CONFIG_HOME first if there's no specified dir"""
-    filestore = fakes.FakeFilestore(test_case[0])
+    filestore = fakes.FakeFilestore(test_case.files)
     dir = config_dir.ConfigDir(filestore, test_env)
-    assert dir.list_layouts() == test_case[1]
+    assert dir.list_layouts() == test_case.expected_layout_names
 
 
 home_dir_tests = [
-    (
-        {"/home/test/.rezide/alpha.toml": "", "/home/test/.rezide/beta.toml": ""},
-        {"alpha", "beta"},
+    DirTestCase(
+        files={"/home/test/.rezide/alpha.toml": "", "/home/test/.rezide/beta.toml": ""},
+        expected_layout_names={"alpha", "beta"},
     ),
-    (
-        {"/home/test/.rezide/hello.toml": "", "/home/test/.rezide/world.toml": ""},
-        {"hello", "world"},
+    DirTestCase(
+        files={
+            "/home/test/.rezide/hello.toml": "",
+            "/home/test/.rezide/world.toml": "",
+        },
+        expected_layout_names={"hello", "world"},
     ),
 ]
 
@@ -99,10 +115,10 @@ def test_lists_layouts_from_home_when_xdg_config_dir_is_empty(test_case, test_en
     """Fall back on $HOME/.rezide/ if there's no config dir path specified and
     $XDG_CONFIG_HOME is empty
     """
-    filestore = fakes.FakeFilestore(test_case[0])
+    filestore = fakes.FakeFilestore(test_case.files)
     env = dtos.Env(home="/home/test/", xdg_config_home="")
     dir = config_dir.ConfigDir(filestore, env)
-    assert dir.list_layouts() == test_case[1]
+    assert dir.list_layouts() == test_case.expected_layout_names
 
 
 @pytest.mark.parametrize("test_case", home_dir_tests)
@@ -110,9 +126,9 @@ def test_lists_layouts_from_home_when_xdg_config_dir_doesnt_exist(test_case, tes
     """Fall back on $HOME/.rezide/ if there's no config dir path specified and
     xdg_config_dir is specified, but doesn't exist
     """
-    filestore = fakes.FakeFilestore(test_case[0])
+    filestore = fakes.FakeFilestore(test_case.files)
     dir = config_dir.ConfigDir(filestore, test_env)
-    assert dir.list_layouts() == test_case[1]
+    assert dir.list_layouts() == test_case.expected_layout_names
 
 
 def test_throws_error_if_unspecified_and_env_vars_fail(test_env):
@@ -133,29 +149,35 @@ def test_throws_error_if_unspecified_and_env_vars_empty(test_env):
         config_dir.ConfigDir(filestore, dtos.Env("", ""))
 
 
+class LayoutFilePathTestCase(NamedTuple):
+    files: Dict[str, str]
+    layout_name_to_find: str
+    expected_path: str
+
+
 layout_file_path_tests = [
-    (
-        {
+    LayoutFilePathTestCase(
+        files={
             "/home/test/.config/rezide/alpha.toml": "",
             "/home/test/.config/rezide/beta.toml": "",
         },
-        "alpha",
-        "/home/test/.config/rezide/alpha.toml",
+        layout_name_to_find="alpha",
+        expected_path="/home/test/.config/rezide/alpha.toml",
     ),
-    (
-        {
+    LayoutFilePathTestCase(
+        files={
             "/home/test/.config/rezide/alpha.toml": "",
             "/home/test/.config/rezide/beta.toml": "",
         },
-        "beta",
-        "/home/test/.config/rezide/beta.toml",
+        layout_name_to_find="beta",
+        expected_path="/home/test/.config/rezide/beta.toml",
     ),
-    (
-        {
+    LayoutFilePathTestCase(
+        files={
             "/home/test/.rezide/yay.toml": "",
         },
-        "yay",
-        "/home/test/.rezide/yay.toml",
+        layout_name_to_find="yay",
+        expected_path="/home/test/.rezide/yay.toml",
     ),
 ]
 
@@ -163,9 +185,12 @@ layout_file_path_tests = [
 @pytest.mark.parametrize("test_case", layout_file_path_tests)
 def test_get_layout_file_path(test_case, test_env):
     """Convert layout names into absolute paths within the config dir"""
-    filestore = fakes.FakeFilestore(test_case[0])
+    filestore = fakes.FakeFilestore(test_case.files)
     dir = config_dir.ConfigDir(filestore, test_env)
-    assert dir.get_layout_file_path(test_case[1]) == test_case[2]
+    assert (
+        dir.get_layout_file_path(test_case.layout_name_to_find)
+        == test_case.expected_path
+    )
 
 
 def test_cant_find_layout(test_env):
